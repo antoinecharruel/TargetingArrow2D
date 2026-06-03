dc7ab75823b0b529d637d7a2bfc8349c81649063
antoine.charruel <charruel.antoine@outlook.com>
2026-06-03 22:38:23 +0200

    refactor: apply Transform2D to sprite positioning in _apply_node_xform

--- addons/targeting_arrow_2d/targeting_arrow_2d.gd
+++ addons/targeting_arrow_2d/targeting_arrow_2d.gd
@@ -54,56 +54,76 @@ func process_arrow(target_position: Vector2) -> void:
 	if _nodes.is_empty():
 		load_sprites()
 		
-	var p0: Vector2 = global_position
-	var p3: Vector2 = target_position
-	var p1: Vector2 = p0 + (p3 - p0) * control_point_factors_p1
-	var p2: Vector2 = p0 + (p3 - p0) * control_point_factors_p2
-	
-	var bezier_positions: Array[Vector2] = []
-	for i in range(total_nodes):
-		var t: float = 1.0 - float(i) / float(total_nodes - 1)
-		var bx: float = pow(1-t,3)*p0.x + 3*pow(1-t,2)*t*p1.x + 3*(1-t)*pow(t,2)*p2.x + pow(t,3)*p3.x
-		var by: float = pow(1-t,3)*p0.y + 3*pow(1-t,2)*t*p1.y + 3*(1-t)*pow(t,2)*p2.y + pow(t,3)*p3.y
-		bezier_positions.append(Vector2(bx, by))
+	var bezier_positions: Array = _compute_bezier_position(target_position)
 	
 	for i in range(total_nodes):
 		if _nodes[i] == null:
 			continue
 		
 		var t_scale := float(i) / float(total_nodes - 1)
-		_apply_node_scale(i, bezier_positions, t_scale)
-
-
-func _apply_node_scale(i: int, bezier_positions: Array, t_scale: float):
-		var scale_factor := lerp(scale_start, scale_end, t_scale)
+		_apply_node_xform(i, bezier_positions, t_scale)
 		
-		if i == 0:
-			_nodes[i].scale = head_scale * scale_factor
+		
+func _compute_bezier_position(target_position) -> Array:
+	var p0: Vector2 = global_position
+	var p3: Vector2 = target_position
+	var p1: Vector2 = p0 + (p3 - p0) * control_point_factors_p1
+	var p2: Vector2 = p0 + (p3 - p0) * control_point_factors_p2
+	
+	var bezier_positions: Array[Vector2] = []
+	
+	for i in range(total_nodes):
+		var t: float = 1.0 - float(i) / float(total_nodes - 1)
+		var bx: float = pow(1-t,3)*p0.x + 3*pow(1-t,2)*t*p1.x + 3*(1-t)*pow(t,2)*p2.x + pow(t,3)*p3.x
+		var by: float = pow(1-t,3)*p0.y + 3*pow(1-t,2)*t*p1.y + 3*(1-t)*pow(t,2)*p2.y + pow(t,3)*p3.y
+		bezier_positions.append(Vector2(bx, by))
+	
+	return bezier_positions
+	
+	
+func _apply_node_xform(i: int, bezier_positions: Array, t_scale: float):
+	var scale_factor := lerp(scale_start, scale_end, t_scale)
+	
+	if i == 0 and total_nodes == 1: # Head only
+		_nodes[i].visible = false
+		return
+		
+	elif i == 0 and total_nodes > 1: # Head
+		_nodes[i].visible = true
+		var dir: Vector2 = bezier_positions[1] - bezier_positions[0]
+		var head_angle: float = signed_angle(Vector2.DOWN, dir)
+
+		var rotation: float = deg_to_rad(head_angle)
+		var rotated_offset: Vector2 = head_texture_offset.rotated(rotation)
+
+		_nodes[i].scale = head_scale * scale_factor
+		var node_scale: Vector2 = _nodes[i].scale
+		var skew: float = 0.0
+		var position: Vector2 = bezier_positions[0] - rotated_offset * node_scale
+		var new_transform := Transform2D(rotation, node_scale, skew, position)
+		_nodes[i].global_transform = new_transform
+
+	else: # Nodes
+		var dir: Vector2 = bezier_positions[i] - bezier_positions[i - 1]
+		var angle: float = signed_angle(Vector2.DOWN, dir)
+
+		var cumulative_dist: float = 0.0
+		for j in range(1, i + 1):
+			cumulative_dist += bezier_positions[j].distance_to(bezier_positions[j - 1])
+
+		if cumulative_dist < cumulative_dist_max:
+			_nodes[i].visible = false
 		else:
+			_nodes[i].visible = true
+
+			var rotation: float = deg_to_rad(angle)
+
 			_nodes[i].scale = nodes_scale * scale_factor
-		
-		if i == 0 and total_nodes > 1: # Head
-			var dir: Vector2 = bezier_positions[1] - bezier_positions[0]
-			var head_angle: float = signed_angle(Vector2.DOWN, dir)
-			_nodes[i].rotation_degrees = head_angle
-			
-			var angle_rad: float = deg_to_rad(head_angle)
-			var rotated_offset: Vector2 = head_texture_offset.rotated(angle_rad)
-			_nodes[i].global_position = bezier_positions[0] - rotated_offset * _nodes[i].scale
-		
-		else: # Nodes
-			var dir: Vector2 = bezier_positions[i] - bezier_positions[i - 1]
-			_nodes[i].rotation_degrees = signed_angle(Vector2.DOWN, dir)
-			
-			var cumulative_dist: float = 0.0
-			for j in range(1, i + 1):
-				cumulative_dist += bezier_positions[j].distance_to(bezier_positions[j - 1])
-			
-			if cumulative_dist < cumulative_dist_max:
-				_nodes[i].visible = false
-			else:
-				_nodes[i].visible = true
-				_nodes[i].global_position = bezier_positions[i]
+			var node_scale: Vector2 = _nodes[i].scale
+			var skew: float = 0.0
+			var position: Vector2 = bezier_positions[i]
+			var new_transform := Transform2D(rotation, node_scale, skew, position)
+			_nodes[i].global_transform = new_transform
 			
 			
 func delete() -> void:
